f4f9d86c95fa07b77a0b6f4e6a92eaf4e42ff355
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-10-21 23:18:50 -0400

    deleted test app and DStore and pyc files
    hooked up the actual recycleapp

--- recycleApp/mainRecycleApp/apps.py
+++ recycleApp/mainRecycleApp/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class WebappConfig(AppConfig):
-    name = 'webapp'
+class MainrecycleappConfig(AppConfig):
+    name = 'mainRecycleApp'
--- recycleApp/webapp/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.conf.urls import url
-from . import views
-
-# Create your views here.
-urlpatterns = [
-	url(r'^$', views.index, name = 'index')]
