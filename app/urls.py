bfa2651d7fd5bd2bb3657cb6c2983475d9387edb
Moustafa <drsh2k19@gmail.com>
2017-10-24 03:40:57 -0400

    add user authetication + create basic views

--- app/urls.py
+++ app/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', views.index, name='index'),
+    url(r'^', include('users.urls', namespace='users')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import logout,authenticate, login
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from . import forms
 
-# Create your views here.
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print (username, password)
+        user = authenticate(username = username, password=password)
+        print (user)
+        if user is None: return HttpResponse("Invalid Login")
+        if request.user.is_authenticated(): return redirect ('/')
+        login(request, user)
+        return redirect ('/')
+
+    return render(request, 'users/login.djhtml')
+
+@csrf_exempt
+def signout(request):
+    logout(request)
+    return redirect('/')
+
+def signup(request):
+    if request.method == 'POST':
+        userForm = forms.SignupForm(request.POST or None)
+        if ( userForm.is_valid()
+             and userForm.cleaned_data['password'] ==
+              userForm.cleaned_data['confirm_password']):
+                user = userForm.save()
+                user.set_password(user.password)
+                user.save()
+                return HttpResponse('signup succcessful\
+                    <a href="/login"> login</a>')
+        else:
+            print (uerForm.errors)
+            return HttpResponse('something wrong!')
+
+
+    context = {
+        'signupForm' : forms.SignupForm
+    }
+    return render(request, 'users/signup.djhtml', context=context)
+
+def profile(request):
+    return render(request, 'users/profile.djhtml')
