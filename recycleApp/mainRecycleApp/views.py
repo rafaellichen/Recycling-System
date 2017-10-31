1ae23e8729992179e7fba4ff6fbeabfc2fc88762
RAFAEL <rafaellichen@gmail.com>
2017-10-31 15:58:21 -0400

    Update views.py

--- recycleApp/mainRecycleApp/views.py
+++ recycleApp/mainRecycleApp/views.py
@@ -5,11 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-<<<<<<< HEAD
     return render(request, 'mainRecycleApp/home.html')
 
 def about(request):
     return render(request, 'mainRecycleApp/about.html')
-=======
-	return render(request, 'mainRecycleApp/home.html')
->>>>>>> 5864c299cd6050e292038c3ece7ca99e0b0fae77
