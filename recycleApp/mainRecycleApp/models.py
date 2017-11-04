028c380b3f3f25f6cede35ae01b823365eaa8dc0
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-11-04 18:22:01 -0400

    Initial work for models of recycling centers

--- recycleApp/mainRecycleApp/models.py
+++ recycleApp/mainRecycleApp/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class RecyclingCenters(models.Model):
+	name = models.CharField()
+	address = models.CharField()
+	phone = models.CharField()
+	website = models.CharField()
+	timetable = models.CharField()
+
+	
