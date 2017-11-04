00475c368e193f1916835c4df4d9b2ed53c62ea9
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-11-04 19:12:15 -0400

    fixed issues with searchresult app

--- recycleApp/searchResult/urls.py
+++ recycleApp/searchResult/urls.py
@@ -6,6 +6,6 @@ from django.views.generic import ListView, DetailView
 from searchResult.models import RecyclingCenters
 
 urlpatterns = [
-    url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("name"),
+    url(r'^$', ListView.as_view(queryset=RecyclingCenters.objects.all().order_by("name"),
     							template_name="searchResult/result.html")),
 ]
