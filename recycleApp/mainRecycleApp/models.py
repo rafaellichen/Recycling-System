bfda9e0c6520c79a0c659821a58812ddbdd664f0
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-11-04 18:24:59 -0400

    Char field replaced with text field

--- recycleApp/mainRecycleApp/models.py
+++ recycleApp/mainRecycleApp/models.py
@@ -4,10 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 class RecyclingCenters(models.Model):
-	name = models.CharField()
-	address = models.CharField()
-	phone = models.CharField()
-	website = models.CharField()
-	timetable = models.CharField()
+	name = models.TextField()
+	address = models.TextField()
+	phone = models.TextField()
+	website = models.TextField()
+	timetable = models.TextField()
 
-	
+	def __str__(self):
+		return self.name	
