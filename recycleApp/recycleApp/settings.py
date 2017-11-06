039cb3c751e1438120441347396c723e206350aa
Moustafa <drsh2k19@gmail.com>
2017-11-05 22:40:58 -0500

    add users app

--- recycleApp/recycleApp/settings.py
+++ recycleApp/recycleApp/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'mainRecycleApp',
     'searchResult',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
