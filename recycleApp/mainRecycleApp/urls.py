e620ffc77d69d4d3769eb7ac392521345ec5ec00
Shirong Zheng <shirongzheng1027@gmail.com>
2017-11-06 00:24:02 -0500

    fix contact urlpattern

--- recycleApp/mainRecycleApp/urls.py
+++ recycleApp/mainRecycleApp/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^/about/$', views.about, name='about'),
+    url(r'^/contact/$', views.contact, name='contact'),
 ]
+
 
 
 
