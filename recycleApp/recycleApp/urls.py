d32c588ed0e6f332b9dfffa575317404696134c7
Moustafa <drsh2k19@gmail.com>
2017-11-05 22:44:04 -0500

    add static files url to rootURLconf

--- recycleApp/recycleApp/urls.py
+++ recycleApp/recycleApp/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from mainRecycleApp.views import about
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('mainRecycleApp.urls')),
-    url(r'^result/$', include('searchResult.urls')),    
+    url(r'^result/$', include('searchResult.urls')),
     url(r'^about$', about),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
