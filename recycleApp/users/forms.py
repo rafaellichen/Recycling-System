ff08da5e93eb5addaafde462cb76e82e51c79f55
Moustafa <drsh2k19@gmail.com>
2017-11-05 22:52:21 -0500

    added signup form

---
+++ recycleApp/users/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django import forms
+
+class SignupForm (forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username', 'password']
