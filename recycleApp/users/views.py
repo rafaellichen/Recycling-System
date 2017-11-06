55751696640e24acb8e194cf322e35c424391a21
Moustafa <drsh2k19@gmail.com>
2017-11-05 22:51:27 -0500

    setup users app controllers structure

--- recycleApp/users/views.py
+++ recycleApp/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import logout,authenticate, login
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+# from . import forms
 
-# Create your views here.
+
+def signin(request):
+    pass
+
+@csrf_exempt
+def signout(request):
+    pass
+def signup(request):
+    pass
+
+def profile(request):
+    pass
