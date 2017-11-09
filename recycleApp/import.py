cf7f6592e853c09683c07a78df46d1610fb24e53
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-11-09 00:47:06 -0500

    Fixing pylint errors

--- recycleApp/import.py
+++ recycleApp/import.py
@@ -1,30 +1,29 @@
-import csv, sys, os
-
+import csv
+import sys
+import os
 import django
+from mainRecycleApp.models import RecyclingCenter
 
 django.setup()
 
+directory = "/recycleApp/"
 
-direct = "/recycleApp/"
-
-sys.path.append(direct)
+sys.path.append(directory)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-from mainRecycleApp.models import RecycleCenter
+csvData = csv.reader(open("DonateNYCCSV.csv"), delimiter=",")
 
-data = csv.reader(open("DonateNYCCSV.csv"), delimiter=",")
-
-for row in data:
-	if row[0] != 'name':
-		recycle = RecycleCenter()
-		recycle.name = row[0]
-		recycle.address = row[1] 
-		recycle.borough = row[2]
-		recycle.state = row[3]
-		recycle.zipcode = row[4]
-		recycle.phone = row[5]
-		recycle.picks_up = row[6]
-		recycle.type = row[7]
-		recycle.url = row[8]
-		recycle.save()
+for row in csvData:
+    if row[0] != 'name':
+        recycle = RecyclingCenter()
+        recycle.name = row[0]
+        recycle.address = row[1]
+        recycle.borough = row[2]
+        recycle.state = row[3]
+        recycle.zipcode = row[4]
+        recycle.phone = row[5]
+        recycle.picks_up = row[6]
+        recycle.type = row[7]
+        recycle.url = row[8]
+        recycle.save()
