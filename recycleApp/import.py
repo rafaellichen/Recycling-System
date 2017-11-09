2e879854af8928372330ee12c50b1139fbe2d951
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-11-08 23:13:00 -0500

    Populated Django model

--- recycleApp/import.py
+++ recycleApp/import.py
@@ -1,3 +1,5 @@
+'use if with django shell
+
 import csv
 
 from mainRecycleApp.models import RecycleCenter
@@ -6,7 +8,7 @@ data = csv.reader(open("DonateNYCCSV.csv"), delimiter=",")
 
 for row in data:
 	if row[0] != 'name':
-		recycle = RecycleCenter()
+		recycle = RecyclingCenter()
 		recycle.name = row[0]
 		recycle.address = row[1] 
 		recycle.borough = row[2]
@@ -14,6 +16,6 @@ for row in data:
 		recycle.zipcode = row[4]
 		recycle.phone = row[5]
 		recycle.picks_up = row[6]
-		recycle.type = row[7]
-		recycle.url = row[8]
+		recycle.recycleType = row[7]
+		recycle.website = row[8]
 		recycle.save()
