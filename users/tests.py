933f7966674aa90db9c7759b51c13f1d1f217e2c
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-11-14 14:50:06 -0500

    Fixed Login Test for Users app

--- users/tests.py
+++ users/tests.py
@@ -2,16 +2,23 @@
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from catalog.forms import SignupForm
 
 class LoginTest(TestCase):
     '''LoginTest for User App'''
     def setUp(self):
         '''Method setup that sets the user credentials'''
-        user = User.objects.create_user(username='test', password: 'password')
+        user = User.objects.create_user(username='test', password='password')
         user.save()            
 
     def test_login(self):
         '''Method test_Login that logs in'''
-        response = self.client.login('/login/', self.credentials)
+        login = self.client.login(username='test', password='password')
+        response = self.client.get('/')
 
+        self.assertEqual(str(response.context['user']), 'test')
+        self.assertEqual(response.status_code, 200)
 
+	def test_logout(self):
+		'''Method test_logout that verifies the logout'''       
