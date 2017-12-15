525a6b825d47c333d4f85ff207cdbf8a4593abb9
Moustafa <drsh2k19@gmail.com>
2017-12-15 15:36:22 -0500

    display bookmarked places in profile page -add remove option

--- mainRecycleApp/admin.py
+++ mainRecycleApp/admin.py
@@ -5,9 +5,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from mainRecycleApp.models import RecyclingCenter, PublicRecyclingBin, SpecialWasteSite, Event, Zip
+from mainRecycleApp.models import RecyclingCenter, PublicRecyclingBin, SpecialWasteSite, Event, Zip, Bookmark
 admin.site.register(RecyclingCenter)
 admin.site.register(PublicRecyclingBin)
 admin.site.register(SpecialWasteSite)
 admin.site.register(Event)
 admin.site.register(Zip)
+admin.site.register(Bookmark)
