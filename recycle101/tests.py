5912fd1400343c7e7520a2ed7352d0d3b5938911
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-12-18 12:57:45 -0500

    Added model value test and setting up Recycle101 with the main app

--- recycle101/tests.py
+++ recycle101/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
+from mainRecycleApp.models import HowTo
+
+class HowToModelTest(TestCase):
+
+    @classmethod
+    def setUp(self):
+        '''This method will set up non-modified objects for How To model
+           and will be used by all test methods'''
+        self.howTo = HowTo.objects.create(recycleType='Test Type',
+                                       description='Test description')
+
+    def test_public_recycle_bin_name(self):
+        """
+        Testing the how to model values
+        """
+        self.assertEqual(self.howTo.borough, "Test Type")
+        self.assertEqual(self.howTo.siteType, "Test description")
+
+    def tearDown(self):
+        del self
--- recycle101/views.py
+++ recycle101/views.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+'''Views for Recycle101'''
 from django.shortcuts import render
+from recycle101.models import HowTo
 
 # Create your views here.
+def index(request):
+    '''Index render method for mainrecycleApp/home'''
+    return render(request, 'main.html')
