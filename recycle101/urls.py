7d965ef118551a36a479057f7e00fa87399c5e0f
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-12-21 15:56:59 -0500

    Added an api route for the autofill ajax method for recycle101

--- recycle101/urls.py
+++ recycle101/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^recycle101', views.index, name='index'),
-    url(r'^search/', views.searchHowTo, name='searchHowTo')    
+    url(r'^recycle101/', views.index, name='index'),
+    url(r'^search101', views.searchHowTo, name='searchHowTo'),
+    url(r'^api/types/', views.getItemTypes, name='getItemTypes')    
 ]
--- recycle101/views.py
+++ recycle101/views.py
@@ -11,8 +11,16 @@ def index(request):
 def searchHowTo(request):
     if request.method == 'POST':
         recycleType = request.POST['recycleType']
-        print (recycleType)
+        # print (recycleType)
         result = list(HowTo.objects.filter(recycleType=recycleType).values())
-        print (result)
+        # print (result)
         return render(request, 'main.html', {"data": result})
-        
+
+def getItemTypes(request):
+    # if the request that is being sent over is a ajax request
+    if request.is_ajax():
+        # get the keyword from the request
+        keyword = request.GET.get('term')
+        # query the database for any entity that is like the keyword
+        queryResult = HowTo.objects.filter(recycleType__icontains = keyword)
+        print (queryResult)
